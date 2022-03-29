259d92f622ecc773d70dc3ba41e31d75f689cf9f
Guilherme Oliveira <49658342+guiolive@users.noreply.github.com>
2022-03-29 19:49:37 -0300

    Delete Uri1002.cs

--- Uri1002.cs
+++
@@ -1,9 +0,0 @@
-using System.Globalization;
-double n = 3.14159;
-double area, raio;
-
-raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-area = (n * Math.Pow(raio, 2.0));
-
-Console.WriteLine("A= " + area.ToString("F4", CultureInfo.InvariantCulture));
