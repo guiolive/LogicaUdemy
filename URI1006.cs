3ed9bfa4c28889e30985cfa24570ea8ffcb8d525
Guilherme Oliveira <49658342+guiolive@users.noreply.github.com>
2022-05-29 18:13:09 -0300

    Create URI1006.cs

---
+++ URI1006.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Globalization;
+
+namespace URI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = ((A * 2) + (B * 3) + (C * 5)) / 10;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F1", CultureInfo.InvariantCulture));
+
+
+
+
+            
+
+        }
+    }
+}
