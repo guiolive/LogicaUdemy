6b39915f08bbb984cc1f86616ce1add6672fb917
Guilherme Oliveira <49658342+guiolive@users.noreply.github.com>
2022-05-29 16:43:03 -0300

    Update URI1003.cs

--- URI1003.cs
+++ URI1003.cs
@@ -1,1 +1,23 @@
+using System;
+using System.Globalization;
 
+namespace URI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            int soma = A + B;
+
+            Console.WriteLine("SOMA = " + soma);
+
+
+
+            
+
+        }
+    }
+}
