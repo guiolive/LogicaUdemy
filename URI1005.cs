508d80346a141147c0549201babb6df5df120fd8
Guilherme Oliveira <49658342+guiolive@users.noreply.github.com>
2022-05-29 17:34:24 -0300

    Create URI1005.cs

---
+++ URI1005.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+
+namespace URI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = ((A * 3.5) + (B * 7.5)) / 11;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F5", CultureInfo.InvariantCulture));
+
+
+
+
+            
+
+        }
+    }
+}
